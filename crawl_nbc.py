94b999a734845ebd90c4888e2386009dcf7f6b3d
suryanshrathor <suryanshr650@gmail.com>
2025-02-25 22:40:52 +0530

    Web scraping using crawl4ai

---
+++ crawl_nbc.py
@@ -0,0 +1,11 @@
+import asyncio
+from crawl4ai import AsyncWebCrawler
+
+
+async def main():
+    async with AsyncWebCrawler(verbose=True) as crawler:
+        result = await crawler.arun(url="https://www.nbcnews.com/business")
+        print(result.markdown)
+
+if __name__ == "__main__":
+    asyncio.run(main())
